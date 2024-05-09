57b7d83997d414244c840fda35b32a97b5319f09
airondevt <airondev@gmail.com>
2024-05-09 11:33:18 +0100

    fix

--- dist/database/contractor/models/contractor.model.js
+++ dist/database/contractor/models/contractor.model.js
@@ -345,9 +345,9 @@ ContractorSchema.methods.getOnboarding = function () {
                         if (stage.status == 2 && hasProfile)
                             stage = { status: 3, label: 'quiz' };
                         // if (stage.status == 3 && hasGstDetails) stage = {status: 4, label: 'quiz'} 
-                        if (stage.status == 4 && hasPassedQuiz)
+                        if (stage.status == 3 && hasPassedQuiz)
                             stage = { status: 4, label: 'stripeAccount' };
-                        if (stage.status == 5 && hasStripeAccount)
+                        if (stage.status == 4 && hasStripeAccount)
                             stage = { status: 5, label: 'done' };
                     }
                     if (this.accountType == 'Employee') {
