192e9ece9bc9a767923ce3206323810234c8b652
RepairFind <156581343+RepairFind-CA@users.noreply.github.com>
2024-05-12 09:15:20 +0100

    Fix: server restart after deployement

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ubuntu/repairfind-be/deploy.log
 
 echo 'pm2 restart repairfind-backend' >> /home/ubuntu/repairfind-be/deploy.log
-# pm2 restart repairfind-backend >> /home/ubuntu/repairfind-be/deploy.log
+sudo pm2 restart repairfind-backend >> /home/ubuntu/repairfind-be/deploy.log
