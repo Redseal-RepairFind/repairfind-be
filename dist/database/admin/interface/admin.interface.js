8333f1b165e1d1df569a9330dbbb9006ddb64ca0
airondev <airondev@gmail.com>
2024-06-14 13:54:03 +0100

    merge

--- dist/database/admin/interface/admin.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AdminStatus = void 0;
-var AdminStatus;
-(function (AdminStatus) {
-    AdminStatus["PENDING"] = "PENDING";
-    AdminStatus["ACTIVE"] = "ACTIVE";
-    AdminStatus["SUSPENDED"] = "SUSPENDED";
-})(AdminStatus || (exports.AdminStatus = AdminStatus = {}));
