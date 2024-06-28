d677d3626ec5796afa42ffb8d89c007258465e04
airondev <airondev@gmail.com>
2024-06-28 11:21:55 +0100

    Email queuing done

---
+++ src/services/bullmq/jobs/send_email.ts
@@ -0,0 +1,21 @@
+import { SendMailOptions } from "nodemailer";
+import { EmailService } from "../..";
+import TransactionModel, { ITransaction, TRANSACTION_STATUS } from "../../../database/common/transaction.model";
+import { IContractor, IContractorCertnDetails } from "../../../database/contractor/interface/contractor.interface";
+import { ContractorModel } from "../../../database/contractor/models/contractor.model";
+import { castPayloadToDTO } from "../../../utils/interface_dto.util";
+import { Logger } from "../../logger";
+import { StripeService } from "../../stripe";
+
+
+export const sendEmail = async (job: any) => {
+    try {
+
+        const mailOptions : SendMailOptions = job.data
+        await EmailService.createTransport().sendMail(mailOptions);
+        Logger.info(`Email sent successfully to ${mailOptions.to} with CC to ${mailOptions.cc}`);
+
+    } catch (error) {
+        Logger.error('Error occurred while syncing certn:', error);
+    }
+};
