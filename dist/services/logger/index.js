dbcb3e85663a76fa4c1e7a9e61e65c1822a728a2
airondev <airondev@gmail.com>
2024-07-04 11:08:34 +0100

    remove dist from git

--- dist/services/logger/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Logger = void 0;
-var winston_1 = require("./winston");
-var Logger = winston_1.Log;
-exports.Logger = Logger;
