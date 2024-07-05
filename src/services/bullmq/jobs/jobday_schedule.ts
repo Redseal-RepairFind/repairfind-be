be2e8d9374c1770d3473a1b838f7da5e44029fcf
airondev <airondev@gmail.com>
2024-07-05 18:27:29 +0100

    new push notification events handled

--- src/services/bullmq/jobs/jobday_schedule.ts
+++ src/services/bullmq/jobs/jobday_schedule.ts
@@ -60,51 +60,56 @@ export const jobDayScheduleCheck = async () => {
                     
 
                     //
-                    if (hourDifference === 1 ) {
+                    if (hourDifference <= 1 ) {
                         if(!job.reminders.includes(JOB_SCHEDULE_REMINDER.HOURS_1)){
                              sendReminderContractor(customer, contractor, job, `You have a job with ${customer.name} scheduled for today ${formattedJobStartDate}`)
                              sendReminderCustomer(customer, contractor, job, `You have a job with ${contractor.name} scheduled for today ${formattedJobStartDate}`)
                              job.reminders.push(JOB_SCHEDULE_REMINDER.HOURS_1) 
                             job.save() 
+                            continue
                         }
                      }
 
                      
 
-                    if (hourDifference === 6 ) {
+                    if (hourDifference <= 6 ) {
                        if(!job.reminders.includes(JOB_SCHEDULE_REMINDER.HOURS_6)){
                             sendReminderContractor(customer, contractor, job, `You have a job with ${customer.name} scheduled for today ${formattedJobStartDate}`)
                             sendReminderCustomer(customer, contractor, job, `You have a job with ${contractor.name} scheduled for today ${formattedJobStartDate}`)
                             job.reminders.push(JOB_SCHEDULE_REMINDER.HOURS_6) 
                             job.save() 
+                            continue
                        }
                     }
 
-                    if (hourDifference === 12) {
+                    if (hourDifference <= 12) {
                         if(!job.reminders.includes(JOB_SCHEDULE_REMINDER.HOURS_12)){
                             sendReminderContractor(customer, contractor, job, `You have a job with ${customer.name} scheduled for today ${formattedJobStartDate}`)
                             sendReminderCustomer(customer, contractor, job, `You have a job with ${contractor.name} scheduled for today ${formattedJobStartDate}`)
                             job.reminders.push(JOB_SCHEDULE_REMINDER.HOURS_12) 
                             job.save() 
+                            continue
                        }
                     }
 
-                    if (hourDifference === 24) {
+                    if (hourDifference <= 24) {
                         if(!job.reminders.includes(JOB_SCHEDULE_REMINDER.HOURS_24)){
                             sendReminderContractor(customer, contractor, job, `You have a job with ${customer.name} scheduled for tomorrow ${jobStartDate.toDateString()}`)
                             sendReminderCustomer(customer, contractor, job, `You have a job with ${contractor.name} scheduled for tomorrow ${jobStartDate.toDateString()}`)
                             job.reminders.push(JOB_SCHEDULE_REMINDER.HOURS_24) 
                             job.save() 
+                            continue
                         }
                         
                     }
 
-                    if (daysDifference === 48) {
+                    if (daysDifference <= 48) {
                         if(!job.reminders.includes(JOB_SCHEDULE_REMINDER.HOURS_48)){
                             sendReminderContractor(customer, contractor, job, `You have a job with ${customer.name} scheduled for ${jobStartDate.toDateString()}`)
                             sendReminderCustomer(customer, contractor, job, `You have a job with ${contractor.name} scheduled for  ${formattedJobStartDate}`) 
                             job.reminders.push(JOB_SCHEDULE_REMINDER.HOURS_48) 
                             job.save() 
+                            continue
                         }                       
                     }
                     
