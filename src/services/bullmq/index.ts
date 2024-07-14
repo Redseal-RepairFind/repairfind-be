8255d824aed9face283bfef725f002950a9506c0
airondev <airondev@gmail.com>
2024-07-14 21:20:02 +0100

    fix on schedule notifications

--- src/services/bullmq/index.ts
+++ src/services/bullmq/index.ts
@@ -81,7 +81,7 @@ class JobQueue {
     this.addJob('expireJobs', {}, { repeat: { every: 7200000 } });
     this.addJob('handleEscrowTransfer', {}, { repeat: { every: 600000 } });
     // this.addJob('jobDayScheduleCheck', {}, { repeat: { cron: '0 0 * * *', tz: 'America/Los_Angeles',} }); //America/Los_Angeles
-    this.addJob('jobDayScheduleCheck', {}, { repeat: { cron: '* * * * *'} }); //every minute
+    // this.addJob('jobDayScheduleCheck', {}, { repeat: { cron: '* * * * *'} }); //every minute
     this.addJob('quizReminderCheck', {}, { repeat: { cron: '0 0 */4 * *'} }); //4 days
   }
 
