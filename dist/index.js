ae5aab98bcdbd147b924c540067d52745f24ff3d
airondev <airondev@gmail.com>
2024-08-08 08:13:25 +0100

    fix

--- dist/index.js
+++ dist/index.js
@@ -128,7 +128,7 @@ twillio_1.default.initialize();
 var InittimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 logger_1.Logger.info("Current timezone: ".concat(InittimeZone));
 // Set the default timezone to PST (Pacific Standard Time)
-// process.env.TZ = 'America/Los_Angeles';
+process.env.TZ = 'America/Los_Angeles';
 // Initialize server
 var port = process.env.PORT || 3000;
 var FirsttimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
